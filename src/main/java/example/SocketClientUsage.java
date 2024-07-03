c8238c124cd6b90432c80b77243a20220b847ef8
nzdmlm <1546521816@qq.com>
2024-07-03 13:01:02 +0800

    加入命令行工具入口，实现命令行工具

--- src/main/java/example/SocketClientUsage.java
+++ src/main/java/example/SocketClientUsage.java
@@ -8,6 +8,7 @@
 package example;
 
 import client.Client;
+import client.CmdClient;
 import client.SocketClient;
 
 public class SocketClientUsage {
@@ -15,10 +16,33 @@ public class SocketClientUsage {
         String host = "localhost";
         int port = 12345;
         Client client = new SocketClient(host, port);
+        CmdClient cmdClient = new CmdClient(client);
+        cmdClient.main();
 //        client.get("zsy1");
-        client.set("zsy12","for test");
-        client.get("zsy12");
-        client.rm("zsy12");
-        client.get("zsy12");
+//        client.set("zsy12","for test");
+//        client.set("zsy12","for test1");
+//        client.set("zsy13","for test");
+//        client.set("zsy14","for test");
+//        client.set("zsy15","for test1");
+//        client.set("zsy16","for test1");
+//        client.set("zsy177","for test");
+//        client.set("zsy18","for test");
+//        client.set("zsy19","for test12");
+//        client.get("zsy12");
+//        client.rm("zsy177");
+//        client.set("zsy12","for test1");
+//        client.set("zsy13","for test");
+//        client.set("zsy14","for test");
+//        client.set("zsy15","for test1");
+//        client.set("zsy16","for test1");
+//        client.set("zsy17","for test");
+//        client.rm("zsy13");
+//        client.rm("zsy14");
+//        client.rm("zsy15");
+//          client.get("zsy12");
+//        client.get("zsy16");
+//        client.get("zsy17");
+//
+//        client.get("zsy15");
     }
 }
